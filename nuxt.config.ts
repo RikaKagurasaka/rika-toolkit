c540d7646bf9e8405ee44a9b39fde086fab7cb8c
RikaKagurazaka <tjj.rikap@gmail.com>
2025-06-13 22:24:08 +0800

    add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,12 @@ export default defineNuxtConfig({
     '@hypernym/nuxt-gsap',
   ],
   css: ["@unocss/reset/tailwind-compat.css", "assets/index.css"],
+  app: {
+    head: {
+      title: "Rika 工具箱",
+      meta: [
+        { name: "description", content: " 有些时候会突然想写一点小工具，但是不知道这个东西写出来有什么用，或者不知道往哪里放。这个项目就是为了解决该问题而诞生的。这里存放了我不知道如何分类的小工具。 " },
+      ],
+    },
+  },
 });
